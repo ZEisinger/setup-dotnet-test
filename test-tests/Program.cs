73213f05233102d7ec7598e513dc281b34b9ed1b
Zachary Eisinger <zeisinger@github.com>
2019-10-17 21:10:36 -0700

    exe depends on exe

---
+++ test-tests/Program.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace test_tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var instance = new test_dotnet.Class();
+            Console.WriteLine(instance.isTrue());
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
